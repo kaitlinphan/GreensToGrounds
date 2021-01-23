bc92595003584397946e64ecdc2d5e5f83b0c931
tchang421 <hc8ff@virginia.edu>
2021-01-22 22:11:31 -0500

    test mission model

--- pages/views.py
+++ pages/views.py
@@ -4,10 +4,11 @@ from django.http import HttpResponse
 from .models import Mission
 
 def homeview(request):
-    #return HttpResponse('Hello, World! Welcome to the new Greens to Grounds website!')
-    if Mission.objects.all().count()>0:
-        mission = Mission.objects.get(pk=1)
-        context = {"missions" : mission}
-    else:
-        context = {"missions" : "no mission statement"}
-    return render(request, 'templates/home.html', context)
+    # return HttpResponse('Hello, World! Welcome to the new Greens to Grounds website!')
+    # if Mission.objects.all().count()>0:
+    #     mission = Mission.objects.get(pk=1)
+    #     context = {"missions" : mission}
+    # else:
+    #     context = {"missions" : "no mission statement"}
+    # return render(request, 'templates/home.html', context)
+    return render(request, 'templates/home.html')
