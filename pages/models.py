036ed7c058f6b7eebd4e0f200e2f7bfe7953bf39
tchang421 <hc8ff@virginia.edu>
2021-01-22 21:55:22 -0500

    fixed mission statement

--- pages/models.py
+++ pages/models.py
@@ -3,11 +3,10 @@ from django.utils import timezone
 from datetime import date
 
 class Mission(models.Model):
-    name = models.CharField(max_length=100)
-    pub_date = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=1000, default="No Description Provided")
+    pub_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.name
+        return self.description
     
 
 class Product(models.Model):
