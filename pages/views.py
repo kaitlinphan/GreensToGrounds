35c6a17b592d83487ce873eec37d19ba8956f59f
tchang421 <hc8ff@virginia.edu>
2021-04-03 23:59:21 -0400

    mission staement fixed

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Mission
 
 def homeview(request):
     # return HttpResponse('Hello, World! Welcome to the new Greens to Grounds website!')
+    mission_statement = Mission.objects.order_by('-pub_date')[:1]
+
+    context = {'mission_statement': mission_statement}
+    return render(request, 'templates/base.html', context)
+    #return HttpResponse(template.render(context, request))
+
     # if Mission.objects.all().count()>0:
     #     mission = Mission.objects.get(pk=1)
     #     context = {"missions" : mission}
     # else:
     #     context = {"missions" : "no mission statement"}
     # return render(request, 'templates/home.html', context)
-    return render(request, 'templates/home.html')
+    
+    #return render(request, 'templates/home.html')
